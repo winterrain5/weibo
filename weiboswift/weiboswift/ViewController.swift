1dd8d0b7f9a7bd4f106c7e1f123b42b94ea76a41
石冬冬 <913419042@qq.com>
2016-07-25 20:27:02 +0800

    初始化项目

---
+++ weiboswift/weiboswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weiboswift
+//
+//  Created by 石冬冬 on 16/7/25.
+//  Copyright © 2016年 sdd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ weiboswift/weiboswiftTests/weiboswiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  weiboswiftTests.swift
+//  weiboswiftTests
+//
+//  Created by 石冬冬 on 16/7/25.
+//  Copyright © 2016年 sdd. All rights reserved.
+//
+
+import XCTest
+@testable import weiboswift
+
+class weiboswiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
