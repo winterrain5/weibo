1a8df144c3f7425916966abc90d8167babfd03af
石冬冬 <913419042@qq.com>
2016-10-24 15:59:32 +0800

    优化配图

---
+++ WeiBo/WeiBo/Classes/Home/CSPictureView.swift
@@ -0,0 +1,13 @@
+//
+//  CSPictureView.swift
+//  WeiBo
+//
+//  Created by 石冬冬 on 16/10/24.
+//  Copyright © 2016年 sdd. All rights reserved.
+//
+
+import UIKit
+
+class CSPictureView: UICollectionView {
+
+}
